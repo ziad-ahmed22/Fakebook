cda36c31269035b495682c600da81d5ad31a6604
Ziad_Ahmed <ziiadd2211@gmail.com>
2023-11-26 16:56:55 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,35 @@ import { Toast } from "./components/Toast/Toast";
 import { Feed } from "./pages/Feed/Feed";
 import { RequireAuth } from "./components/RequireAuth/RequireAuth";
 import { RequireBack } from "./components/RequireBack/RequireBack";
+import { useEffect } from "react";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    // loader: <p>loading...</p>,
-    // errorElement: <p>error</p>,
-    children: [
-      {
-        element: <RequireBack />,
-        children: [{ index: true, element: <Home /> }],
-      },
-      {
-        element: <RequireAuth />,
-        children: [{ path: "feed", element: <Feed /> }],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      // loader: <p>loading...</p>,
+      // errorElement: <p>error</p>,
+      children: [
+        {
+          element: <RequireBack />,
+          children: [{ index: true, element: <Home /> }],
+        },
+        {
+          element: <RequireAuth />,
+          children: [{ path: "feed", element: <Feed /> }],
+        },
+      ],
+    },
+  ],
+  { basename: "/Fakebook" }
+);
 
 export const App = () => {
+  useEffect(() => {
+    alert("This Project Is Under Development");
+  }, []);
+
   return (
     <>
       <RouterProvider router={router} />
